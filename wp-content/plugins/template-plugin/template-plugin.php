dc87968a5fa276515fceb45ee0461c2d807ba1db
Jason Crist <jcrist@pbking.com>
2025-05-27 10:10:25 -0400

    Add basic theme and plugin directories and wp-env configuration

---
+++ wp-content/plugins/template-plugin/template-plugin.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Plugin Name:       Template Plugin Customizations
+ * Description:       Customizations for the Template Site. 
+ * Version:           1.0
+ * Author:            twentybellows 
+ * Text Domain:       template-plugin
+ */
+
+if (! defined('ABSPATH')) {
+	exit; // Exit if accessed directly.
+}
